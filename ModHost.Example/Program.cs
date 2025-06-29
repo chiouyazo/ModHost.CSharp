de5025f3459df5010995f3bb579dd204eaa333eb
Max-Leon Ernst <chiouyazo@gmail.com>
2025-06-29 19:59:16 +0200

    Updated example project to include docs example commands

--- ModHost.Example/Program.cs
+++ ModHost.Example/Program.cs
@@ -39,7 +39,7 @@ public class Program
 		CommandHandler clientComm = bridge.GetClientCommandHandler();
 
 		CommandBuilder testDataCommand = commandHandler.CreateCommand("testdata")
-			.AddArgument("name", "string", true, "player_names")
+			.AddArgument("name", "string", true, "CUSTOM:player_names")
 			.Executes(async context =>
 			{
 				await context.SendFeedback("Test feedback");
@@ -52,14 +52,73 @@ public class Program
 
 		await commandHandler.RegisterCommandAsync(testDataCommand);
 		
+		
 		commandHandler.RegisterSuggestionProvider("player_names", async (completion, ctx) =>
 		{
+			// completion would be the string that the user has already typed in the chat. (Can be used to filter/sort)
 			List<string> playerNames = await ctx.GetPlayerNames();
 			
 			return playerNames;
-			// return list.Where(x => x.StartsWith(completion));
-			// await commandHandler.FinalizeSuggestion(ctx.ContextId); // TODO: This has to be done after the response was send
 		});
+
+		CommandBuilder commandWithCustomSuggestions = commandHandler.CreateCommand("command_with_custom_suggestions")
+			.AddArgument("player_name", "string", false, "CUSTOM:player_names")
+			.Executes(ExecuteCommandWithCustomSuggestions);
+
+		await commandHandler.RegisterCommandAsync(commandWithCustomSuggestions);
+
+		CommandBuilder commandWithSuggestions = commandHandler.CreateCommand("command_with_suggestions")
+			.AddArgument("entity", "string", false, "SUMMONABLE_ENTITIES")
+			.Executes(ExecuteCommandWithSuggestions);
+
+		await commandHandler.RegisterCommandAsync(commandWithSuggestions);
+
+
+		CommandBuilder testCommand = commandHandler.CreateCommand("test_command")
+			.Executes(ExecuteTestCommand);
+
+		await commandHandler.RegisterCommandAsync(testCommand);
+
+		CommandBuilder requiredCommand = commandHandler.CreateCommand("required_command")
+			.Requires(async source => await source.HasPermissionLevel(1))
+			.Executes(ExecuteRequiredCommand);
+
+		await commandHandler.RegisterCommandAsync(requiredCommand);
+
+
+		CommandBuilder subCommandOne = commandHandler.CreateCommand("sub_command_one")
+			.Executes(ExecuteSubCommandOne);
+
+		CommandBuilder commandOne = commandHandler.CreateCommand("command_one")
+			.AddSubCommand(subCommandOne);
+		
+		await commandHandler.RegisterCommandAsync(commandOne);
+
+
+		CommandBuilder subCommandTwo = commandHandler.CreateCommand("sub_command_two")
+			.Executes(ExecuteSubCommandTwo);
+
+		CommandBuilder commandTwo = commandHandler.CreateCommand("command_two")
+			.Executes(ExecuteCommandTwo)
+			.AddSubCommand(subCommandTwo);
+		
+		await commandHandler.RegisterCommandAsync(commandTwo);
+
+		
+
+		CommandBuilder commandWithArg = commandHandler.CreateCommand("command_with_arg")
+			.Executes(ExecuteCommandWithArg)
+			.AddArgument("value", "integer");
+
+		await commandHandler.RegisterCommandAsync(commandWithArg);
+
+		CommandBuilder commandWithTwoArgs = commandHandler.CreateCommand("command_with_two_args")
+			.Executes(ExecuteCommandWithMultipleArgs)
+			.AddArgument("value_one", "integer")
+			.AddArgument("value_two", "integer", optional: true);
+
+		await commandHandler.RegisterCommandAsync(commandWithTwoArgs);
+		
 		
 		CommandBuilder dayCommand = commandHandler.CreateCommand("day")
 			.Executes(async context =>
@@ -74,5 +133,65 @@ public class Program
 		Console.WriteLine("ModHost ready and connected.");
 
 		await Task.Delay(-1);
+	}
+
+	private static async Task ExecuteCommandWithCustomSuggestions(CommandContext context)
+	{
+		string name = context.Payload["player_name"];
+		await context.SendFeedback($"Called /command_with_custom_suggestions with value = {name}.");
+	}
+
+	private static async Task ExecuteCommandWithSuggestions(CommandContext context)
+	{
+		string entityType = context.Payload["entity"];
+		await context.SendFeedback($"Called /command_with_suggestions with entity {entityType}.");
+	}
+
+	private static async Task ExecuteCommandWithMultipleArgs(CommandContext context)
+	{
+		if (context.Payload.Count == 1)
+		{
+			// We can safely assume this exists, because it is not optional.
+			string valueOne = context.Payload["value_one"];
+			await context.SendFeedback($"Called /command_with_two_args with value one = {valueOne}.");
+		}
+		else
+		{
+			string valueOne = context.Payload["value_one"];
+			string valueTwo = context.Payload["value_two"];
+			await context.SendFeedback($"Called /command_with_two_args with value one = {valueOne} and value two = {valueTwo}.");
+		}
+	}
+
+	private static async Task ExecuteCommandWithArg(CommandContext context)
+	{
+		// It can be safely assumed that this is a integer, otherwise minecraft wouldn't execute it.
+		string value = context.Payload["value"];
+		await context.SendFeedback($"Called /command_with_arg with value = {value}.");
+	}
+
+	private static async Task ExecuteCommandTwo(CommandContext context)
+	{
+		await context.SendFeedback("Called /command_two");
+	}
+
+	private static async Task ExecuteSubCommandTwo(CommandContext context)
+	{
+		await context.SendFeedback("Called /sub_command_two");
+	}
+
+	private static async Task ExecuteSubCommandOne(CommandContext context)
+	{
+		await context.SendFeedback("Called /sub_command_one");
+	}
+
+	private static async Task ExecuteRequiredCommand(CommandContext context)
+	{
+		await context.SendFeedback("Called /required_command");
+	}
+
+	private static async Task ExecuteTestCommand(CommandContext context)
+	{
+		await context.SendFeedback("Called /test_command");
 	}
 }
